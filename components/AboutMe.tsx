ceb4d6ecb906d3b814d7590f41645c9e87fc7264
saddamBD <saddam.ruet@gmail.com>
2024-11-12 14:18:12 +0600

    fine tuning

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -6,9 +6,9 @@ function AboutMe() {
       <h2 className="text-3xl font-semibold mb-4">About Me</h2>
       <p className="text-lg text-muted-foreground">
         👋 Hey there! Iam Saddam Hossain, a full-stack software engineer from
-        Bangladesh 🇨🇴 with over three (3) years of professional experience. I
-        focus on details and Iam passionate about crafting software products
-        that look great and are both accessible and easy to maintain.
+        Bangladesh with over three (3) years of professional experience. I focus
+        on details and Iam passionate about crafting software products that look
+        great and are both accessible and easy to maintain.
       </p>
     </section>
   );
